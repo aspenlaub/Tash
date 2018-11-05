3bba6dd100c8a7ffc9766713f1016de19e96a126
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-11-05 17:58:32 +0100

    Additional process status "does not exist" + possiblity for a task to fail with an error message

--- src/Model/ControllableProcessStatus.cs
+++ src/Model/ControllableProcessStatus.cs
@@ -1,6 +1,6 @@
 ﻿// ReSharper disable UnusedMember.Global
 namespace Aspenlaub.Net.GitHub.CSharp.Tash.Model {
     public enum ControllableProcessStatus {
-        Idle = 0, Busy = 1, Dead = 2
+        Idle = 0, Busy = 1, Dead = 2, DoesNotExist = 3
     }
 }
--- src/Model/ControllableProcessTask.cs
+++ src/Model/ControllableProcessTask.cs
@@ -13,11 +13,13 @@ namespace Aspenlaub.Net.GitHub.CSharp.Tash.Model {
         [JsonConverter(typeof(StringEnumConverter))]
         public ControllableProcessTaskType Type { get; set; }
 
-        public string ControlName { get; set; }
+        public string ControlName { get; set; } = "";
 
-        public string Text { get; set; }
+        public string Text { get; set; } = "";
 
         [JsonConverter(typeof(StringEnumConverter))]
         public ControllableProcessTaskStatus Status { get; set; }
+
+        public string ErrorMessage { get; set; } = "";
     }
 }
